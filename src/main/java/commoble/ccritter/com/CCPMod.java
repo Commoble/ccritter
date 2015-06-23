8f67752c9e8249b465c40076b93db5288cadcc4c
Commoble <commoble@gmail.com>
2015-06-22 22:19:56 -0500

    Updated version numbers

--- src/main/java/commoble/ccritter/com/CCPMod.java
+++ src/main/java/commoble/ccritter/com/CCPMod.java
@@ -17,7 +17,7 @@ public class CCPMod
 	public static CCPMod instance = new CCPMod();
 
     public static final String MODID = "ccritter";
-    public static final String VERSION = "1.7.10-0.2.0.3";
+    public static final String VERSION = "1.7.10-0.2.5";
     public static final String NAME = "CCritter";
     
     @SidedProxy(clientSide="commoble.ccritter.client.CombinedClientProxy", serverSide="commoble.ccritter.server.DedicatedServerProxy")
